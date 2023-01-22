16ee46502fa9e4885abaec6c8b24ba8ea75b32f1
btopo <bret.topo@gmail.com>
2023-01-22 17:53:04 -0500

    Added CharacterController &Json Converter

--- Controllers/Models/RpgClass.cs
+++ Controllers/Models/RpgClass.cs
@@ -1,5 +1,8 @@
+using System.Text.Json.Serialization;
+
 namespace Dotnet5_RPG.Controllers.Models
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum RpgClass
     {
         Knight,
