8e0294b20cd0b8e10e26475207ab79e2944a1999
btopo <bret.topo@gmail.com>
2023-01-22 19:00:27 -0500

    Added AddCharacter Method

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -30,6 +30,13 @@ namespace Dotnet5_RPG.Controllers
         {
             return Ok(characters.FirstOrDefault(c => c.Id == id));
         }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return Ok(characters);
+        }
     }
 
 
